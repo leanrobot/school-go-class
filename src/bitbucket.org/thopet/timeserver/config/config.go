be001e9ee6c05db70f47a24bae8e470eaff60c25
Tom Petit <thopet@uw.edu>
2015-02-09 20:07:47 -0800

    added flags for simulating load

--- src/bitbucket.org/thopet/timeserver/config/config.go
+++ src/bitbucket.org/thopet/timeserver/config/config.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 	log "github.com/cihub/seelog"
 
 )
@@ -18,9 +19,17 @@ const (
 )
 
 var (
+	// the port the timeserver serves HTTP on.
 	Port int
+
+	// Flags related to communicating with the authserver.
 	AuthPort int
 	AuthUrl string
+
+	// Flags related to simulating load.
+	AvgResponseMs time.Duration
+	DeviationMs time.Duration
+
 	VersionPrint bool
 	TemplatesDir string
 	LogConfigFile string
@@ -49,6 +58,16 @@ func initFlags() {
 		"The network address for the auth server")
 	flag.IntVar(&AuthPort, "authport", 9090, 
 		"The port which to connect to the authserver on.")
+
+	// Flags related to simulating load.
+	avgResponseMs := flag.Int("avg-response-ms", 700,
+		`The average amount of duration in milliseconds to wait in order
+		to simulate load`)
+	deviationMs := flag.Int("deviation-ms", 100,
+		`The value of one unit of standard deviation from the
+		average response.`)
+	AvgResponseMs := avgResponseMs * time.Millisecond
+	DeviationMs := deviationMs * time.Millisecond
 
 	flag.Parse()
 }
